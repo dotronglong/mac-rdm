29ffb83552cd973d5d063ef9148617b5454512d3
Long Do <long.do@audiencemedia.com>
2016-08-04 14:13:11 +0700

    Init application

---
+++ rdm/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  rdm
+//
+//  Created by Long Do on 8/4/16.
+//  Copyright © 2016 Long Do. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
